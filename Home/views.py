6172798318d748cbfbcd8435fee1e2b4e000e709
DeiberBedoya <deiberbedoya19@gmail.com>
2026-06-22 23:01:00 -0500

    feat: agregar correo de confirmacion al usuario al realizar la reserva

--- Home/views.py
+++ Home/views.py
@@ -1,21 +1,47 @@
 from django.shortcuts import render, redirect
 from Menu.models import Plato
 from .models import Reserva
+from django.core.mail import send_mail
+from django.conf import settings
+from django.template.loader import render_to_string
 
 def home(request):
     if request.method == 'POST':
-        Reserva.objects.create(
-            nombre   = request.POST.get('nombre'),
-            telefono = request.POST.get('telefono'),
-            fecha    = request.POST.get('fecha'),
-            hora     = request.POST.get('hora'),
-            personas = request.POST.get('personas'),
-            email    = request.POST.get('email'),
-            nota     = request.POST.get('nota'),
-        )
-        return redirect('Home')  # ← redirige y limpia el POST
+            nombre   = request.POST.get('nombre')
+            telefono = request.POST.get('telefono')
+            fecha    = request.POST.get('fecha')
+            hora     = request.POST.get('hora')
+            personas = request.POST.get('personas')
+            email    = request.POST.get('email')
+            nota     = request.POST.get('nota')
 
-    platos_destacados = Plato.objects.filter(disponible=True)[:4]
-    return render(request, "home/home.html", {
-        "platos": platos_destacados,
-    })
+            #Guardar la reserva en la base de datos
+            Reserva.objects.create(
+                nombre=nombre, telefono=telefono, fecha=fecha, hora=hora, personas=personas, email=email, nota=nota, 
+            )
+
+            #Si el cliente escribió su email, le enviamos el correo
+            if email:
+                cuerpo_html = render_to_string('home/correo_confirmacion.html',{
+                    'nombre': nombre,
+                    'fecha': fecha,
+                    'hora': hora,
+                    'personas': personas,
+                    'nota': nota,
+                })
+                #Se le envia el correo
+                send_mail(
+                    subject = 'Confirmacion de reserva - Azotea Rooftop',
+                    message = f'Hola{nombre}, tu reserva para el {fecha} a las {hora} fue recibida.',
+                    from_email = settings.DEFAULT_FROM_EMAIL,
+                    recipient_list = [email],
+                    html_message = cuerpo_html,
+                    fail_silently = False
+                )
+
+            return redirect('Home')
+    
+    Platos_destacados = Plato.objects.filter(disponible=True)[:4]
+    return render(request, "home/home.html",{
+        "platos": Platos_destacados,
+    })        
--- azotea/settings.py
+++ azotea/settings.py
@@ -138,3 +138,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #Configuración para la carpeta media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+#Configuracion del correo - Gmail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
