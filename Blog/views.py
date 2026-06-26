75b90c1626710ab5757b3c1ea3da6fe120945fcf
DeiberBedoya <deiberbedoya19@gmail.com>
2026-06-26 18:14:50 -0500

    feat: agregar paginacion a menu y galeria. validaciones en formulario de contacto

--- Blog/views.py
+++ Blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from Blog.models import Foto
+
 # Create your views here.
 def blog(request):
-    fotos = Foto.objects.all()
+    #Leer la categoria seleccionada de la URL (?categoria = ambiente)
+    categoria = request.GET.get('categoria', None)
+
+    if categoria:
+        fotos = Foto.objects.filter(categoria=categoria)
+    else:
+        fotos = Foto.objects.all()
 
     #paginacion
     paginator = Paginator(fotos, 9)
@@ -13,4 +20,5 @@ def blog(request):
     return render(request, "blog/blog.html", {
         "fotos": page_obj,
         "page_obj": page_obj,
+        "categoria": categoria, #para saber que filtro esta activo
     })
