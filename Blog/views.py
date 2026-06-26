3b678682008a5bda71bfc647ef4ec68c99c22690
DeiberBedoya <deiberbedoya19@gmail.com>
2026-06-26 00:38:42 -0500

    wip: paginacion en menu y galeria - en proceso

--- Blog/views.py
+++ Blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from Blog.models import Foto
 # Create your views here.
 def blog(request):
-    fotos    = Foto.objects.all()
+    fotos = Foto.objects.all()
+
+    #paginacion
+    paginator = Paginator(fotos, 9)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+
     return render(request, "blog/blog.html", {
-        "fotos":    fotos
+        "fotos": page_obj,
+        "page_obj": page_obj,
     })
